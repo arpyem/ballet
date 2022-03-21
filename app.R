b46bcfbe31d3842997b9d404ffcd945f0f2d76fa
arpyem <arpyemofficial@gmail.com>
2022-03-21 02:55:21 +0000

    form 990 data

--- app.R
+++ app.R
@@ -1,11 +1,11 @@
-library(shiny)
-
-ui <- fluidPage(
-  div('wow')
-)
-
-server <- function(input, output, session) {
-  
-}
-
-shinyApp(ui, server)
+# library(shiny)
+# 
+# ui <- fluidPage(
+#   div('wow')
+# )
+# 
+# server <- function(input, output, session) {
+#   
+# }
+# 
+# shinyApp(ui, server)
